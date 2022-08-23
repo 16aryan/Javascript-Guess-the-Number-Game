d0d3d5ada4ddcc859e781c38ae9bd24642d387c4
Aryan Goel <96868069+16aryan@users.noreply.github.com>
2022-08-23 20:17:47 +0530

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log(document.querySelector(".message").textContent); //strips down the text from the html
+document.querySelector(".message").textContent = "Correct Number 🎊🎉";
+document.querySelector(".text").textContent = 13;
+document.querySelector(".score").textContent = 20;
